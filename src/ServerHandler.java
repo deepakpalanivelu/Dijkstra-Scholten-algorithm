7c0289f3b432d2f2dcf7dfcf43344b3ce86ea73b
deepakpalanivelu <rtpdeepak@gmail.com>
2017-03-06 00:18:36 -0800

    Add files via upload

--- src/ServerHandler.java
+++ src/ServerHandler.java
@@ -1,6 +1,5 @@
 package djikstraSchloten;
 
-
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -8,6 +7,7 @@ public class ServerHandler extends SimpleChannelInboundHandler<Message> {
 	Clock clock;
 	Tree tree;
 	String state;
+	
 	ServerHandler(Clock clock,Tree tree) {
 		this.clock = clock;
 		this.tree = tree;
@@ -16,24 +16,20 @@ public class ServerHandler extends SimpleChannelInboundHandler<Message> {
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		super.channelActive(ctx);
-
 	}
+
 	@Override
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-
 		super.channelInactive(ctx);
 	}
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws Exception {
-		// TODO Auto-generated method stub
 		if(msg.getMessage().equals("IDLE")) {
 			System.out.println("Ack recieved from child  "+ msg.getSender());
 			tree.setAckCount(tree.getAckCount() + 1);
-			System.out.println("in node" + tree.getNodeID() + " message sent count = " + tree.getMsgCount() + " acknowlwdge count = " + tree.getAckCount()  );
-
 		}
+
 		else {
 			System.out.println("clock before updating" +clock.getClockTime());
 			System.out.println("_____________RECIEVE EVENT______________");
